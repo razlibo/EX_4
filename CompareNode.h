850f8f5f9ea86e3c871bd45f94df5f2e0fcda4a4
raz <raz.leibovitch1@gmail.com>
2022-12-28 19:53:21 +0200

    push

--- CompareNode.h
+++ CompareNode.h
@@ -5,10 +5,21 @@
 #ifndef EX_4_COMPARENODE_H
 #define EX_4_COMPARENODE_H
 
+#include "queue"
+#include "HuffmanNode.h"
 
-class CompareNode {
+class compareNode{
+private:
+    priority_queue<HuffmanNode*, vector<HuffmanNode*>,	compareNode> pQueue;
+public:
+    bool operator()(HuffmanNode* const & n1, HuffmanNode* const & n2)
+    {
+        return n1->frequency > n2->frequency;
+    }
+
 
 };
+
 
 
 #endif //EX_4_COMPARENODE_H
--- HuffmanNode.h
+++ HuffmanNode.h
@@ -4,10 +4,14 @@
 
 #ifndef EX_4_HUFFMANNODE_H
 #define EX_4_HUFFMANNODE_H
-
+#include "CompareNode.h"
 
 class HuffmanNode {
+private:
+    int a;
 
+public:
+    friend class CompareNode;
 };
 
 
